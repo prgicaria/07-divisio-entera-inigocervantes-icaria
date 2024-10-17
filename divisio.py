0917da4d68b368b3f1b109b39e6cc3497143f303
icervantes <icervantes@instituticaria.cat>
2024-10-17 09:54:29 +0200

    .

--- divisio.py
+++ divisio.py
@@ -1,9 +1,8 @@
-print("Diguem el dividend de la divisió")
-dividend = int(input())
-print("Diguem el divisor de la divisió")
-divisor = int(input())
-quocient = divident / divisor
-residu = divident % divisor
-print("divisió:" divisió)
-print("quocient:" quocient)
-print("residu:" residu)
+dividend = int(input("Diguem el dividend de la divisió: "))
+divisor = int(input("Diguem el divisor de la divisió: "))
+quocient = dividend // divisor  # Divisió entera per obtenir el quocient sencer
+residu = dividend % divisor
+
+print(f"Divisió: {dividend}/{divisor}")
+print(f"Quocient: {quocient}")
+print(f"Residu: {residu}")
