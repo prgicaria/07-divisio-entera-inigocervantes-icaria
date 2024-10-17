bf3c550a3730697189cc7aae7fce46a4e42d4eaf
icervantes <icervantes@instituticaria.cat>
2024-10-17 09:28:38 +0200

    .

---
+++ divisio.py
@@ -0,0 +1,9 @@
+print("Diguem el dividend de la divisió")
+dividend = int(input())
+print("Diguem el divisor de la divisió")
+divisor = int(input())
+quocient = divident / divisor
+residu = divident % divisor
+print("divisió:" divisió)
+print("quocient:" quocient)
+print("residu:" residu)
